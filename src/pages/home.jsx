bbf1e1deea6038997e652db2eca7ca539834c1a8
Ibrohimbek-Muhammadmusayev-new <ibrohimbekmuhammadmusayev34@gmail.com>
2024-09-21 20:16:25 +0500

    create message portfoleo

---
+++ src/pages/home.jsx
@@ -0,0 +1,52 @@
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase/index";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+    const [data, setData] = useState([]);
+    const [search, setSearch] = useState('');
+    
+    const getData = async () => {
+        const querySnapshot = await getDocs(collection(db, "massage"));
+        const data = [];
+        querySnapshot.forEach((doc) => {
+            data.push(doc.data());
+        });
+        setData(data);
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    const searchdata = data.filter((item) => {
+        return item.name.toLowerCase().includes(search.toLowerCase()),
+        item.number.toLowerCase().includes(search.toLowerCase())
+    });
+
+    return (
+        <div className="w-full min-h-[100vh] bg-slate-800">
+            <div className="max-w-[800px] mx-auto h-full">
+                <div className="flex justify-between items-center py-3 px-2">
+                    <h1 className="text-[25px] font-bold text-white">Portfolio messages</h1>
+                    <input onChange={(e) => setSearch(e.target.value)} className="border border-white rounded p-1" type="text" placeholder="search message"/>
+                </div>
+                <div className="flex flex-col gap-[1px] px-4">
+                    <h1 className="text-[20px] font-bold text-white">All Message ( {searchdata.length} )</h1>
+                    {searchdata?.map((item, index) => (
+                        <div key={index} className="bg-slate-600 rounded-md border border-white p-3 my-3">
+                            <div className="flex justify-between">
+                                <p className="font-bold text-white">Gmail: {item.email}</p>
+                                <h1 className="text-slate-300 font-bold flex gap-[10px]"><span className="text-white">Tel:</span>{item.number}</h1>
+                            </div>
+                                <h1 className="font-bold text-white">Name: {item.name}</h1>
+                            <div className="w-full font-light text-wrap text-white h-[80px] rounded p-1 border border-slate-400">
+                                <p>{item.message}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
